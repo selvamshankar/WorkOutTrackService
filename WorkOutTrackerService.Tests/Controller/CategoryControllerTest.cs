a1227d091f2e7094b4e21eba28daf8e0508df487
selvamshankar <38043983+selvamshankar@users.noreply.github.com>
2018-04-03 08:37:03 -0700

    Unit  Test project Changes
    Unit Test Project changes

--- WorkOutTrackerService.Tests/Controller/CategoryControllerTest.cs
+++ WorkOutTrackerService.Tests/Controller/CategoryControllerTest.cs
@@ -1,13 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WorkOutDBLayer;
+using WorkOutDBModel.Model;
 
 namespace WorkOutTrackerService.Tests.Controller
 {
-    class CategoryControllerTest
+    [TestClass]
+    public class CategoryControllerTest
     {
+        [TestMethod()]
+        public void GetAllCategories()
+        {
+            CateogryDb obj = new CateogryDb();
+            int actualCount = obj.GetAll().Count;
+            int expectedCount = 2;
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [TestMethod()]
+        public void GetCategory_By_CategoryId()
+        {
+            CateogryDb obj = new CateogryDb();
+            string actualCategoryName = obj.GetById(1).CategoryName;
+            string expectedCategoryName = "Walk";
+            Assert.AreEqual(expectedCategoryName, actualCategoryName);
+        }
+
+        [TestMethod()]
+        public void AddCategory()
+        {
+            CateogryDb obj = new CateogryDb();
+            bool result= obj.Add(
+                new Category {  CategoryName="Running Test",CategoryId=0});
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod()]
+        public void Delete_Category()
+        {
+            CateogryDb obj = new CateogryDb();
+            bool result = obj.Delete(2);
+            Assert.AreEqual(true, result);
+        }
 
     }
 }
---
+++ WorkOutTrackerService.Tests/Controller/WorkOutTrackerControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WorkOutDBLayer;
+using WorkOutDBModel.Model;
+using System.Linq;
+using System;
+
+namespace WorkOutTrackerService.Tests.Controller
+{
+    public class WorkOutTrackerControllerTest
+    {
+        [TestMethod()]
+        public void GetAllWorkoutTracker()
+        {
+            WorkOutDb obj = new WorkOutDb();
+            int actualCount = obj.GetAll().Count;
+            int expectedCount = 2;
+            Assert.AreEqual(expectedCount, actualCount);
+        }
+
+        [TestMethod()]
+        public void GetWorkouttracker_By_Id()
+        {
+            WorkOutDb obj = new WorkOutDb();
+            string actualworkOutTitle = obj.GetById(1).WorkOutTitle;
+            string expectedWorkOutTitle = "test Workout Tracker";
+            Assert.AreEqual(actualworkOutTitle, expectedWorkOutTitle);
+        }
+
+        [TestMethod()]
+        public void AddWorkOut_Tracker()
+        {
+            WorkOutDb obj = new WorkOutDb();
+            bool result = obj.Add(
+                new WorkOut { Created=DateTime.Now });
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod()]
+        public void Delete_workoutTracker()
+        {
+            WorkOutDb obj = new WorkOutDb();
+            bool result = obj.Delete(2);
+            Assert.AreEqual(true, result);
+        }
+    }
+}
